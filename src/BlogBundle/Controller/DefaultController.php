1fc7cf281ec1e9bd405c86c167b84e550093dbda
Michal Turemka <michal.turemka@gmail.com>
2016-10-18 20:35:23 +0200

    Use database values in real page template

--- src/BlogBundle/Controller/DefaultController.php
+++ src/BlogBundle/Controller/DefaultController.php
@@ -2,6 +2,7 @@
 
 namespace BlogBundle\Controller;
 
+use AdminBundle\Entity\Footer;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
@@ -12,6 +13,15 @@ class DefaultController extends Controller
      */
     public function indexAction()
     {
-        return $this->render('BlogBundle:Default:index.html.twig');
+        $doctrine = $this->getDoctrine();
+        $repo = $doctrine->getRepository('AdminBundle:Footer');
+
+        $query = $repo->createQueryBuilder('f')->select('f.attr, f.value')->getQuery();
+        $result = $query->getResult();
+        $attrs = array_map(function($value) { return $value['attr']; }, $result);
+        $values = array_map(function($value) { return $value['value']; }, $result);
+        $output = array_combine($attrs, $values);
+
+        return $this->render('BlogBundle:Default:index.html.twig', $output);
     }
 }
