92108f5b429dc0ef94cfd1cf6796475324b22524
Michał Turemka <michal.turemka@gmail.com>
2016-12-05 14:59:20 +0100

    New button to upload files

--- web/admin/js/script.js
+++ web/admin/js/script.js
@@ -20,3 +20,14 @@ $('#file_upload_form_file').change(function () {
     var name = $(this)[0].files[0]['name'];
     $('#filename-upload').html(name);
 });
+
+var enlarge_anim_time = 250;
+$('#label-upload').hover(
+    function(){
+        $(this).children("img").stop().animate({width: "+=30", height: "+=30"}, enlarge_anim_time);
+        $(this).stop().animate({ top: "-=15", width: "+=30"}, enlarge_anim_time);
+    },
+    function(){
+        $(this).children("img").stop().animate({width: "-=30", height: "-=30"}, enlarge_anim_time);
+        $(this).stop().animate({top: "+=15", width: "-=30"}, enlarge_anim_time);
+});
