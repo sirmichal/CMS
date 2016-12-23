4b50000e6d8f5bbca4526f0c2728b9605bfa79f4
Michal Turemka <michal.turemka@gmail.com>
2016-12-23 13:21:03 +0100

    Fixes in frontend

--- web/js/script.js
+++ web/js/script.js
@@ -30,10 +30,28 @@ $('.big-post-content').each(function () {
     $(this).text(shortenedText);
 })
 
-var navbar_items = $('.nav.navbar-nav li').length;
-$('.nav.navbar-nav li').width(100/navbar_items + '%');
+// var navbar_items = $('.nav.navbar-nav li');
+// navbar_items.width(100/navbar_items.length + '%');
 
 $('.footer-arrow').click(function () {
     var body = $('body');
     body.stop().animate({ scrollTop: 0 }, '400', 'swing');
+});
+
+$('.sidebar-single-entry').hover(
+    function () {
+        $(this).children('.sidebar-single-entry-arrow').stop().animate({'background-color': 'yellow'}, 150);
+    },
+    function () {
+        $(this).children('.sidebar-single-entry-arrow').stop().animate({'background-color': 'gray'}, 150);
+                    $('.big-post-thumbnail').animate({rotate: '+=10deg'}, 0);
+
+    }
+);
+
+$('.big-post-thumbnail-img').hover(function() {
+    $(this).toggleClass('transition');
+
+}, function() {
+    $(this).toggleClass('transition');
 });
