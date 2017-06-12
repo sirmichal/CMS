6827e09c6910f7efda3a81fb37b342c459134377
Michał Turemka <michal.turemka@gmail.com>
2017-06-12 13:05:42 +0200

    Added deploy.sh script

---
+++ deploy.sh
@@ -0,0 +1,6 @@
+composer install --no-scripts --no-suggest
+npm install --production --prefix web
+HTTPDUSER=$(ps axo user,comm | grep -E '[a]pache|[h]ttpd|[_]www|[w]ww-data|[n]ginx' | grep -v root | head -1 | cut -d\  -f1)
+mkdir var
+sudo setfacl -dR -m u:"$HTTPDUSER":rwX -m u:$(whoami):rwX var
+sudo setfacl -R -m u:"$HTTPDUSER":rwX -m u:$(whoami):rwX var
