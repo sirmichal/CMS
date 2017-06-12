a86852abb4188726211e39a78d43aaf7a0a40284
Michał Turemka <michal.turemka@gmail.com>
2017-06-12 12:47:24 +0200

    Updated composer.lock and gulpfile.js, package.json moved to web directory

--- gulpfile.js
+++ gulpfile.js
@@ -4,20 +4,25 @@ var sftp = require('gulp-sftp');
 
 var src = [
     '**',
-    '!web/bundles/*',
+    '!web/node_modules',
+    '!web/.htaccess',
     '!vendor/**',
     '!node_modules/**',
     '!tests/**',
     '!nbproject/**',
-    '.ftppass'
+    '!var/**',
+    '!gulpfile.js',
+    '!phpunit.xml.dist',
+    '!LICENSE',
+    '!README.md',
     ];
 
 gulp.task('upload', function () {
     return gulp.src(src)
         .pipe(sftp({
-            host: '192.168.56.101',
+            host: '192.168.56.102',
             auth: 'key',
-            remotePath: '/var/www/gulp/'
+            remotePath: '/var/www/cms'
         }));
 });
 
